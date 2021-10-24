ddec10b3becaa2d695b086722925d7006aedb40c
amustafayev <afgan.mustafayev@pashabank.az>
2021-10-24 20:23:53 +0400

    qdqedeq

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -3,27 +3,35 @@
 module.exports = ({github, context,core}) => {
     
 
-        if(context.payload.pull_request.changed_files > 40){
-            await github.rest.issues.createComment({
-                    issue_number: context.issue.number,
-                    owner: context.repo.owner,
-                    repo: context.repo.repo,
-                    body: 'There is not enough file to create pr'
-            })
-            
-            core.setFailed('Workflow Failed!')
-        }
-
-        const regex = new RegExp(/^(^[A-Z]+)-(\d+):([ A-z1-9\.\,]+)$/);
-
-        if(regex.test(context.payload.pull_request.title)==false){
-            await github.rest.issues.createComment({
+    const MAIN_BRANCH = "main"
+    console.log(context.payload.pull_request.base.ref)
+    if(context.payload.pull_request.changed_files > 40){
+        await github.rest.issues.createComment({
                 issue_number: context.issue.number,
                 owner: context.repo.owner,
                 repo: context.repo.repo,
-                body: 'Title is not in a correct format'
-            })
+                body: 'There is not enough file to create pr'
+        })
         
-            core.setFailed('Workflow Failed!')
-        }
+        core.setFailed('Workflow Failed!')
+    }
+
+
+    var regex = new RegExp(/^(^[A-Z]+)-(\d+):([ A-z1-9\.\,]+)$/);
+
+    if(context.payload.pull_request.base.ref == MAIN_BRANCH){
+        regex = new RegExp(/^RELEASE$/);
+    }
+
+    if(regex.test(context.payload.pull_request.title)==false){
+        await github.rest.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: 'Title is not in a correct format'
+        })
+    
+        core.setFailed('Workflow Failed!') 
+    } 
+
 }
