3707d2c3048cfe9d206c6756eacb74fb0c8d17a3
amustafayev <afgan.mustafayev@pashabank.az>
2021-10-26 12:33:11 +0400

    Script refactored

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -2,19 +2,18 @@
 
 module.exports = async ({github, context, core}) => {
 
-
-    
-
     const MAIN_BRANCH = "main"
     const FILE_LIMIT_EXCEED = '❌There is so many files to create PR!❌'
     const TITLE_FORMAT_ERROR = '❌Title is not in a correct format!❌'
     const PR_BODY_EMPTY_ERROR = '❌PR Body can not be empty!❌'
     const MAX_CHANGED_FILES = 40
-    const MASTER_TITLE_REGEX = new RegExp(/^RELEASE$/) 
-    var REGEX = new RegExp(/^(^[A-Z]+)-(\d+):([ A-z1-9\.\,]+)$/);
+    const MASTER_TITLE_REGEX = /^RELEASE$/ 
+    const TITLE_REGEX = /^(^[A-Z]+)-(\d+):([ A-z1-9\.\,]+)$/
+
+    var regex = new RegExp(TITLE_REGEX);
 
     if(context.payload.pull_request.base.ref == MAIN_BRANCH){
-        REGEX = MASTER_TITLE_REGEX;
+        regex = new RegExp(MASTER_TITLE_REGEX);
     }
 
     if(context.payload.pull_request.changed_files > MAX_CHANGED_FILES){
@@ -28,7 +27,7 @@ module.exports = async ({github, context, core}) => {
         core.setFailed('Workflow Failed! cause: '+ FILE_LIMIT_EXCEED)
     }
 
-    if(REGEX.test(context.payload.pull_request.title)==false){
+    if(regex.test(context.payload.pull_request.title)==false){
         await github.rest.issues.createComment({
             issue_number: context.issue.number,
             owner: context.repo.owner,
